198c752324b59a31657f96fbb6d57ebd97c48b22
ESPORTS-135\cre <t-shirai@oca.ac.jp>
2022-11-15 16:24:12 +0900

    一通り終わった

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,7 +15,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::where('status', false)->get();
         
         return view('tasks.index', compact('tasks'));
     }
@@ -42,9 +42,9 @@ class TaskController extends Controller
         $rules = [
             'task_name' => 'required|max:100',
         ];
-
+         
         $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
-
+         
         Validator::make($request->all(), $rules, $messages)->validate();
 
 
@@ -94,7 +94,41 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
+
         //
+        if($request->status === null) {
+            $rules = [
+                'task_name' => 'required|max:100'
+            ];
+
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+
+            validotor::make($request->all(), $rules, $messages)->validate();
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
+
+            //データベースに保存
+            $task->save();
+        } else {
+            //「完了」ボタンを押したとき
+
+            //該当のタスクを検索
+            $task = Task::find($id);
+
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+
+            //データベースに保存
+            $task->save();
+        }
+        
+        
+        //リダイレクト
+        return redirect('/tasks');
     }
 
     /**
@@ -105,7 +139,9 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Task::find($id)->delete();
+
+        return redirect('/tasks');
     }
 }
 
